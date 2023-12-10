a1766b4f97fd6fe7d78d346848648d2b720060cd
func0der <529819+func0der@users.noreply.github.com>
2023-12-10 20:59:19 +0100

    Add proper meta script for playbook running.

---
+++ bin/apply_development.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+SCRIPT_DIR=$(cd $(dirname $0) && pwd)
+
+"$SCRIPT_DIR/run_ansible.sh" pb.development.yml $@
--- bin/run_ansible.sh
+++ bin/run_ansible.sh
@@ -1,17 +1,16 @@
-#!/usr/bin/env sh
-
-set -x
+#!/usr/bin/env bash
 
 SCRIPT_DIR=$(cd $(dirname $0) && pwd)
 
 PLAYBOOK="$1"
+ANSIBLE_ADDITIONAL_PARAMETERS="${@:2}"
 ANSIBLE_ROOT=$(cd "$SCRIPT_DIR/../ansible" && pwd)
 
 cd "$ANSIBLE_ROOT"
 
-if [ -z $1 ] || [ ! -f $PLAYBOOK ]; then
+if [ -z $PLAYBOOK ] || [ ! -f $PLAYBOOK ]; then
   echo 'Provide valid playbook file. Playbook "'$PLAYBOOK'" not found.'
   exit 1
 fi
 
-ansible-playbook --ask-become-pass -vvvv $PLAYBOOK
+ansible-playbook --ask-become-pass $ANSIBLE_ADDITIONAL_PARAMETERS "$PLAYBOOK"
