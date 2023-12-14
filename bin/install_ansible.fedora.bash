e8abda08a38a425dd7af412a284ae2e9af0f6aeb
func0der <529819+func0der@users.noreply.github.com>
2023-12-14 23:00:50 +0100

    Add Fedora installer.

---
+++ bin/install_ansible.fedora.bash
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+#####
+# Script adapted from: https://docs.ansible.com/ansible/latest/installation_guide/installation_distros.html#installing-ansible-on-fedora-linux
+#
+
+sudo dnf install -y ansible
